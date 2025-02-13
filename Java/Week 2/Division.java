f7af9780e626c9d9b4376aa82d545af1d84d2eed
sanjeev <112648813+isanjeevv@users.noreply.github.com>
2025-02-13 07:56:47 +0530

    Update Division.java

--- Java/Week 2/Division.java
+++ Java/Week 2/Division.java
@@ -1,1 +1,18 @@
+public class DivisionWithoutOperator {
 
+    public static void main(String[] args) {
+        int dividend = 17;
+        int divisor = 3;
+
+        int quotient = 0;
+        int remainder = dividend;
+
+        while (remainder >= divisor) {
+            remainder -= divisor;
+            quotient++;
+        }
+
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+    }
+}
