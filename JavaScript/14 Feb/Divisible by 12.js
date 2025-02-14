ad368558999a4b606ae77d28381aa40117d6684c
sanjeev <112648813+isanjeevv@users.noreply.github.com>
2025-02-14 14:13:49 +0530

    Add files via upload

---
+++ JavaScript/14 Feb/Divisible by 12.js
@@ -0,0 +1,10 @@
+// Check for a number, if its divisible by 12 using if statement
+
+number = 12
+
+if (number % 12 == 0){
+    console.log("The number " + number + " is divisible by 12")
+}
+else{
+    console.log("The number is not divisible by 12")
+}
